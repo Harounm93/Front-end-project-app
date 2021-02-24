30ded644a27826c9370332ee63be97eb6cfbcc5a
harounm93 <mahdiharoun93@gmail.com>
2021-02-24 13:52:04 +0000

    button changed

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -2,7 +2,7 @@ function ListItem(props) {
   console.log("item is here");
   const {text, deleteItem, index} = props
 
-  return (<li > {text} <button onClick= {()=> deleteItem(index)}> delete hobby </button>
+  return (<li > {text} <button onClick= {()=> deleteItem(index)}> Delete</button>
   
  </li>
   
